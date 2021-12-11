f35be1334db5f92e0cb4beb80b36aa9e438649f0
heppokofrontend <heppokofrontend@gmail.com>
2021-12-12 00:13:19 +0900

    🥒 Add Ecma Kawarabe

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { dictMaker } from '@heppokofrontend/dictionary-file-maker';
+import kawarabeEcma from './members/kawarabe-ecma.js';
 import { format } from './format';
 
 const dict = [
+  ...format(kawarabeEcma),
 ];
 
 dictMaker(dict, 'win', './downloads'); // The text file is writen.
