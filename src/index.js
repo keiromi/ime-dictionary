c28ec17a0728a55f89cef7c254d0120296d41fbc
heppokofrontend <heppokofrontend@gmail.com>
2021-12-12 00:27:07 +0900

    ☂️ Add Oruko Amase

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { dictMaker } from '@heppokofrontend/dictionary-file-maker';
+import amaseOruko from './members/amase-oruko.js';
 import alabamiAsuki from './members/akabami-asuki.js';
 import kawarabeEcma from './members/kawarabe-ecma.js';
 import { format } from './format';
 
 const dict = [
+  ...format(amaseOruko),
   ...format(alabamiAsuki),
   ...format(kawarabeEcma),
 ];
---
+++ src/members/amase-oruko.js
@@ -0,0 +1,16 @@
+import '../type.d';
+
+/** @type {LiverData} - 雨瀬おるこの辞書データ */
+export default ({
+  name: ['あませ おるこ', '雨瀬 おるこ'],
+  alias: [
+  ],
+  marks: ['☂️'],
+  tags: [
+    '#切り抜きおるこちゃん', // 切り抜き
+    '#おるこちゃんFA', // ファンアート
+    '#おるこちゃん見ないで', // NSFWファンアート
+  ],
+  fans: [],
+  twitter: ['@Amase_Oruko'],
+});
